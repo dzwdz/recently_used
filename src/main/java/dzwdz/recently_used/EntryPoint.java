e7b6f1646324327f0cf367e63a5825b016149d9e
Dźwiedź <kg67199@gmail.com>
2020-08-25 13:40:41 +0200

    Filter out items already on the hotbar

--- src/main/java/dzwdz/recently_used/EntryPoint.java
+++ src/main/java/dzwdz/recently_used/EntryPoint.java
@@ -4,23 +4,37 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 
 import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Environment(EnvType.CLIENT)
 public class EntryPoint implements ClientModInitializer {
-    public static final int HISTORY_LENGTH = 45;
+    public static final int HISTORY_LENGTH = 54;
     public static LinkedList<ItemStack> recent = new LinkedList<ItemStack>();
 
     public static final ItemGroup RECENT_GROUP = FabricItemGroupBuilder.create(
             new Identifier("recently_used", "recently_used"))
             .icon(() -> new ItemStack(Items.COBWEB))
             .appendItems(stacks -> {
-                for (ItemStack s : recent) stacks.add(s);
+                Stream<ItemStack> stream = recent.stream();
+                PlayerEntity player = MinecraftClient.getInstance().player;
+                if (player != null) {
+                    for (ItemStack hotbarStack : player.inventory.main.subList(0, 9)) {
+                        if (hotbarStack.isEmpty()) continue;
+                        ItemStack copy = hotbarStack.copy();
+                        copy.setCount(1);
+                        stream = stream.filter((ItemStack other) -> !ItemStack.areEqual(copy, other));
+                    }
+                }
+                stacks.addAll(stream.limit(45).collect(Collectors.toList()));
             })
             .build();
 
@@ -33,7 +47,7 @@ public class EntryPoint implements ClientModInitializer {
         ItemStack copy = stack.copy();
         copy.setCount(1);
 
-        recent.removeIf((ItemStack other) -> {return ItemStack.areEqual(copy, other);});
+        recent.removeIf((ItemStack other) -> ItemStack.areEqual(copy, other));
         recent.addFirst(stack);
         if (recent.size() > HISTORY_LENGTH) recent.removeLast();
     }
