bd9036d91b06422aea6c7431d938c5d11571c1f9
Dźwiedź <kg67199@gmail.com>
2020-08-31 20:57:36 +0200

    Finished the trash button

---
+++ src/main/java/dzwdz/recently_used/TrashButton.java
@@ -0,0 +1,50 @@
+package dzwdz.recently_used;
+
+import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
+import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
+import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Text;
+
+public class TrashButton extends TexturedButtonWidget {
+    private CreativeInventoryScreen screen;
+
+    public TrashButton(int x, int y, AbstractInventoryScreen screen, Text text) {
+        super(x, y, 8, 8, 0, 0, 8, EntryPoint.TRASH_ICON, 8, 16, new TrashButtonAction(screen), text);
+        this.screen = (CreativeInventoryScreen) screen;
+    }
+
+    private boolean shouldShow() {
+        return screen.getSelectedTab() == EntryPoint.RECENT_GROUP.getIndex();
+    }
+
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        if (shouldShow()) super.render(matrices, mouseX, mouseY, delta);
+    }
+
+    public void renderButton(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        super.renderButton(matrices, mouseX, mouseY, delta);
+        if (this.isHovered())
+            screen.renderTooltip(matrices, getMessage(), mouseX, mouseY);
+    }
+
+    public void onPress() {
+        if (shouldShow()) super.onPress();
+    }
+}
+
+class TrashButtonAction implements ButtonWidget.PressAction {
+    private CreativeInventoryScreen screen;
+
+    public TrashButtonAction(AbstractInventoryScreen screen) {
+        this.screen = (CreativeInventoryScreen) screen;
+    }
+
+    @Override
+    public void onPress(ButtonWidget button) {
+        EntryPoint.recent.clear();
+        screen.getScreenHandler().itemList.clear();
+        screen.getScreenHandler().scrollItems(0);
+    }
+}
--- src/main/java/dzwdz/recently_used/mixin/CreativeInventoryScreenMixin.java
+++ src/main/java/dzwdz/recently_used/mixin/CreativeInventoryScreenMixin.java
@@ -1,17 +1,16 @@
 package dzwdz.recently_used.mixin;
 
 import dzwdz.recently_used.EntryPoint;
-import dzwdz.recently_used.TrashButtonAction;
+import dzwdz.recently_used.TrashButton;
 import net.minecraft.client.gui.screen.ingame.AbstractInventoryScreen;
 import net.minecraft.client.gui.screen.ingame.CreativeInventoryScreen;
-import net.minecraft.client.gui.widget.TexturedButtonWidget;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.screen.ScreenHandler;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.screen.slot.SlotActionType;
-import net.minecraft.text.LiteralText;
 import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -34,6 +33,6 @@ public abstract class CreativeInventoryScreenMixin extends AbstractInventoryScre
     @Inject(at = @At("TAIL"),
             method = "init")
     private void createButton(CallbackInfo callbackInfo) {
-        addButton(new TexturedButtonWidget(x + 180, y + 6, 8, 8, 0, 0, 8, EntryPoint.TRASH_ICON, 8, 16, new TrashButtonAction(this), new LiteralText("trash")));
+        addButton(new TrashButton(x + 177, y + 6, this, new TranslatableText("recently_used.trashButton")));
     }
 }
